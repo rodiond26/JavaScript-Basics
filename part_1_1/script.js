aff99d3850338b34c903ed48a502d45ed1376d23
Rodion D <rodiond26@gmail.com>
2020-10-20 11:24:57 +0500

    start js self educationn

---
+++ part_1_1/script.js
@@ -0,0 +1,17 @@
+// console.log('Hello!');
+
+// console.log('Hello World!');
+
+// console.warn('Hello World!');
+
+// console.error('Hello World!');
+
+// console.clear();
+
+// console.log('Console clear');
+
+alert('hi there');
+
+// prompt('Enter');
+
+// confirm();
---
+++ part_1_3_String/script.js
@@ -0,0 +1,13 @@
+// экранирование кавычки \'
+
+const firstName = 'John';
+const secondName = 'Smith';
+
+console.log('Hello, ' + firstName + ' ' + secondName);
+
+// использование шаблонной строки
+console.log(`Hello there, ${firstName} ${secondName}`);
+
+const word = prompt('Enter your name');
+const len = word.length;
+console.log(len);
